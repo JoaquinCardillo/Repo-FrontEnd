46e303f3c99d96020c9819aa0a80dc0637e83050
TomiSach <tomassachini@hotmail.com>
2023-05-06 16:38:25 -0300

    funca

--- be/googlesheets.js
+++ be/googlesheets.js
@@ -108,7 +108,7 @@ console.log(products);
     console.log(`${row[0]},${row[1]}, ${row[2]}, ${row[3]}, ${row[4]},${row[5]},${row[6]},${row[7]}, `);
   });
 }
-
-authorize().then(listMajors).catch(console.error);
 */
+authorize().then(listMajors).catch(console.error);
+
 
