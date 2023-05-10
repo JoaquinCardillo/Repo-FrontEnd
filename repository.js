f8d7617afe5a2d3860325600dfe9424da4008d6d
TomiSach <tomassachini@hotmail.com>
2023-05-10 14:22:17 -0300

    n

---
+++ repository.js
@@ -0,0 +1,147 @@
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1HcvQj9dEcavmjp92LtbhVeXTwOZyQlTXhRvkEMhfO_Q/edit#gid=0
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+
+
+///////////////READ
+
+
+async function read(auth) {
+    const sheets = google.sheets({version: 'v4', auth});
+    const res = await sheets.spreadsheets.values.get({
+      spreadsheetId: '1HcvQj9dEcavmjp92LtbhVeXTwOZyQlTXhRvkEMhfO_Q',
+      range: 'stock!A2:I'
+    });
+    
+    const rows = res.data.values;
+  
+    const products = rows.map((row) => ({ 
+      id: +row[0],
+      brand: row[1],
+      price: +row[2],
+      model: row[3],
+      years: +row[4],           //el + hace que lo leea como number
+      kms: +row[5],
+      fuel: row[6],
+      description: row[6],
+      photo: row[7],
+  }));
+  
+  return products;
+  }
+
+////////WRITE
+async function write(spreadsheetId, range, valueInputOption, _values) {
+  const {GoogleAuth} = require('google-auth-library');
+  const {google} = require('googleapis');
+
+  const auth = new GoogleAuth({
+    scopes: 'https://www.googleapis.com/auth/spreadsheets',
+  });
+
+  const service = google.sheets({version: 'v4', auth});
+  let values = products.map((p) => [p.id,p.brand,p.model]);
+  const resource = {
+    values,
+  };
+  try {
+    const result = await service.spreadsheets.values.update({
+      spreadsheetId:"1HcvQj9dEcavmjp92LtbhVeXTwOZyQlTXhRvkEMhfO_Q",
+      range:"stock!A2:I",
+      valueInputOption:"RAW",
+      resource,
+    });
+    console.log('%d cells updated.', result.data.updatedCells);
+    return result;
+  } catch (err) {
+    
+    throw err;
+  }
+}
+
+////////READ & WRITE
+
+async function readAndWrite(){
+  const products=await read();
+  products[0].stock=50;
+  await write(products);
+}
+
+
+
+  
+  authorize().then(read).catch(console.error);
+
+  module.exports = {
+    read,
+    write,
+  };
